6643cf1da13228ca0d79664fc908c99d0a5f8578
AkihiroAurora <AkihiroAurora@proton.me>
2024-09-12 01:14:19 +0330

    updated nvim

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -20,11 +20,11 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button('f', '󰱼 Find file', ':Telescope find_files <CR>'),
-      dashboard.button('n', ' New file', ':ene <BAR> startinsert <CR>'),
-      dashboard.button('t', ' Find text', ':Telescope live_grep <CR>'),
-      dashboard.button('c', ' Config', ':e ~/.config/nvim/init.lua <CR>'),
-      dashboard.button('q', ' Quit', ':qa<CR>'),
+      dashboard.button('f', '󰱼  > Find file', ':Telescope find_files <CR>'),
+      dashboard.button('n', '  > New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('t', '  > Find text', ':Telescope live_grep <CR>'),
+      dashboard.button('c', '  > Config', ':e ~/.config/nvim/init.lua <CR>'),
+      dashboard.button('q', '  > Quit', ':qa<CR>'),
     }
 
     -- Send config to alpha
