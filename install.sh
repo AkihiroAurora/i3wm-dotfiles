03f65298d8e67f42a800ef0deb10f9f78550b3d8
AkihiroAurora <159726685+AkihiroAurora@users.noreply.github.com>
2024-09-13 23:17:22 +0330

    added install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+if ! command -v yay
+then
+  pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+  cd ..
+  rm -r yay
+fi
+yay -Syuq --noconfirm --needed neovim zoxide i3-wm autotiling polybar flameshot ttf-cascadia-code-nerd dunst rofi i3lock kitty fzf zsh oh-my-posh fastfetch btop feh lf cava
+cd ~/dotfiles
+stow --dotfiles . 
